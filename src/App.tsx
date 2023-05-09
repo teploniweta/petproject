ecce7278d531f52ffecd8f50e6f72c81fb6b1d7d
teinawa <helepoctb123@gmail.com>
2023-05-09 18:46:54 +0300

    add router, add lazy loading and chunks

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import { Counter } from "./components/Counter";
+import { Routes, Route } from "react-router-dom";
 import './index.scss';
+import { Link } from "react-router-dom";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { Suspense } from "react";
 
 export const App = () => {
   return (
     <div className="app">
-      123123123
-      <Counter />
+      
+      <Link to={'/'}>Главная</Link>
+      <Link to={'/about'}>О нас</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={'/about'} element={<AboutPageAsync />} />
+          <Route path={'/'} element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   )
 }
---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,7 @@
+export const MainPage = () => {
+  return (
+    <div>
+      MAIN PAGE
+    </div>
+  )
+}
