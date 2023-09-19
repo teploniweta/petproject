7e272c218bffe8bc0119569031489bc40ba6c6f2
teinawa <helepoctb123@gmail.com>
2023-09-19 03:50:04 +0300

    add first widget - navbar, add first ui component - link

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,16 @@
-import {Link} from 'react-router-dom';
 import './styles/index.scss';
 import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
 import { AppRouter } from './providers/router';
+import { Navbar } from 'widgets/Navbar';
 
 const App = () => {
    const { theme, toggleTheme } = useTheme();
 
     return (
         <div className={classNames('app', {}, [theme])}>
+            <Navbar />
             <button onClick={toggleTheme}>TOGGLE</button>
-            <Link to={'/'}>Главная</Link>
-            <Link to={'/about'}>О сайте</Link>
             <AppRouter />
         </div>
     );
--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,9 +1,9 @@
 type Mods = Record<string, boolean | string>
 
-export function classNames(className: string, mods: Mods, additional: string[]): string {
+export function classNames(className: string, mods: Mods = {}, additional: string[] = []): string {
   return [
     className,
-    ...additional, 
+    ...additional.filter(Boolean), 
     ...Object.entries(mods).filter(([className, value]) => Boolean(value))
     .map(([className, value]) => className)
   ].join(' ');
