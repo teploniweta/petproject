dd700ee306418bd00c6b697e5a177c912213c312
teinawa <helepoctb123@gmail.com>
2023-09-19 05:27:16 +0300

    add navpanel, themeswitcher component, button component, add svg loader, file loader

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,14 +3,15 @@ import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
 import { AppRouter } from './providers/router';
 import { Navbar } from 'widgets/Navbar';
+import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
 
 const App = () => {
-   const { theme, toggleTheme } = useTheme();
+   const { theme } = useTheme();
 
     return (
         <div className={classNames('app', {}, [theme])}>
             <Navbar />
-            <button onClick={toggleTheme}>TOGGLE</button>
+            <ThemeSwitcher />
             <AppRouter />
         </div>
     );
---
+++ src/shared/ui/Button/Button.tsx
@@ -0,0 +1,23 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import cls from './Button.module.scss';
+import { ButtonHTMLAttributes, FC } from 'react';
+
+export enum ThemeButton {
+  CLEAR = 'clear'
+}
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  className?: string;
+  theme?: ThemeButton;
+}
+
+export const Button: FC<ButtonProps> = (props) => {
+
+  const {children, className, theme, ...otherProps} = props;
+
+  return (
+    <button {...otherProps} className={classNames(cls.Button, {}, [className, cls[theme]])}>
+      {children}
+    </button>
+  );
+};
