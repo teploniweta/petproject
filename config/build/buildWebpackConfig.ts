43e393ca322e3e9a294a249b69c2389246a18b7b
teinawa <helepoctb123@gmail.com>
2023-11-30 04:24:16 +0300

    fix error with git

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -1,4 +1,5 @@
 import webpack from 'webpack';
+import path from 'path';
 import { BuildOptions } from './types/config';
 import { buildPlugins } from './buildPlugins';
 import { buildLoaders } from './buildLoaders';
