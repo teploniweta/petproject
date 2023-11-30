9b48fa9ac983ee7cbc1f181a8d3844878b6649ec
teinawa <helepoctb123@gmail.com>
2023-11-30 05:37:47 +0300

    try fix ts-error

--- src/pages/AboutPage/ui/AboutPage.stories.tsx
+++ src/pages/AboutPage/ui/AboutPage.stories.tsx
@@ -11,6 +11,7 @@ export default {
         backgroundColor: { control: 'color' },
     },
 } as ComponentMeta<typeof AboutPage>;
+// @ts-ignore
 // eslint-disable-next-line react/jsx-props-no-spreading
 const Template: ComponentStory<typeof AboutPage> = (args) => <AboutPage {...args} />;
 
--- src/pages/MainPage/ui/MainPage.stories.tsx
+++ src/pages/MainPage/ui/MainPage.stories.tsx
@@ -12,6 +12,7 @@ export default {
     },
 } as ComponentMeta<typeof MainPage>;
 
+// @ts-ignore
 // eslint-disable-next-line react/jsx-props-no-spreading
 const Template: ComponentStory<typeof MainPage> = (args) => <MainPage {...args} />;
 
