a4ae3f29ac08f7e4482883bb5371469770d84376
teinawa <helepoctb123@gmail.com>
2023-12-08 20:04:48 +0300

    add auth, add custom text component

--- json-server/index.js
+++ json-server/index.js
@@ -9,6 +9,7 @@ const router = jsonServer.router(path.resolve(__dirname, 'db.json'));
 server.use(jsonServer.defaults({}));
 server.use(jsonServer.bodyParser);
 
+// Нужно для небольшой задержки, чтобы запрос проходил не мгновенно, имитация реального апи
 server.use(async (req, res, next) => {
     await new Promise((res) => {
         setTimeout(res, 800);
@@ -16,6 +17,7 @@ server.use(async (req, res, next) => {
     next();
 });
 
+// Эндпоинт для логина
 server.post('/login', (req, res) => {
     try {
         const { username, password } = req.body;
@@ -37,6 +39,7 @@ server.post('/login', (req, res) => {
     }
 });
 
+// проверяем, авторизован ли пользователь
 // eslint-disable-next-line
 server.use((req, res, next) => {
     if (!req.headers.authorization) {
@@ -48,6 +51,7 @@ server.use((req, res, next) => {
 
 server.use(router);
 
+// запуск сервера
 server.listen(8000, () => {
     console.log('server is running on 8000 port');
 });
--- src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -1,4 +1,5 @@
 import { Modal } from 'shared/ui/Modal/Modal';
+import { classNames } from 'shared/lib/classNames/classNames';
 import { LoginForm } from '../LoginForm/LoginForm';
 
 interface LoginModalProps {
@@ -9,6 +10,7 @@ interface LoginModalProps {
 
 export const LoginModal = ({ className, isOpen, onClose }: LoginModalProps) => (
     <Modal
+        className={classNames('', {}, [className])}
         isOpen={isOpen}
         onClose={onClose}
         lazy
